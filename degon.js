7c35baf2064239820ce6c909d998d4e0836ab6f9
bcbin <bcbin@wo.cn>
2017-09-03 00:33:49 +0800

    initial degon project

---
+++ degon.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         degon
+// @namespace    http://degon.ittun.com
+// @version      0.1
+// @updateURL    http://degon.ittun.com/update.php
+// @downloadURL  http://degon.ittun.com/degon.js
+// @description  degon drug drug price maintain
+// @author       bcbon@wo.cn
+// @include	 *	
+// @grant	GM_getValue
+// @grant	GM_setValue
+// @grant	GM_deleteValue
+// @grant	GM_listValues
+// @grant	GM_setClipboard
+// ==/UserScript==
+
+(function() {
+    'use strict';
+	//console.log(name);	
+	//hello();
+	//unsafeWindow.hello();
+	console.log("hello, world!");
+}
+)();
+
