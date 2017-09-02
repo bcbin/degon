8bc3e7824fe8c595ed1c5c6fac5cf856e40892a3
bcbin <bcbin@yahoo.com>
2017-09-02 12:33:06 -0500

    Update degon.js

--- degon.js
+++ degon.js
@@ -1,14 +1,1 @@
-// ==UserScript==
-// @name         degon
-// @namespace    http://degon.ittun.com
-// @version      0.1
-// @description  degon drug drug price maintain
-// @author       bcbon@wo.cn
-// @include		https://www.baidu.com*	
-// ==/UserScript==
 
-
-(function() {
-	'use strict';
-	console.log("hello, world!");
-})();
