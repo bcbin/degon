cedaa7cccea083d5f44facabf186c61c115ae540
bcbin <bcbin@yahoo.com>
2017-09-03 09:52:47 +0800

    Update heloworld.user.js

--- heloworld.user.js
+++ heloworld.user.js
@@ -1,6 +1,22 @@
+// ==UserScript==
+// @name         hellogit
+// @namespace    http://degon.ittun.com
+// @version      0.1
+// @description  hello golang
+// @author       bcbon@wo.cn
+// @include	 *	
+// @grant	GM_getValue
+// @grant	GM_setValue
+// @grant	GM_deleteValue
+// @grant	GM_listValues
+// @grant	GM_setClipboard
+// ==/UserScript==
 
-bbbbbb
-aaaaaa
-cccccc
-dd
-ff
+(function() {
+    'use strict';
+	//console.log(name);	
+	//hello();
+	//unsafeWindow.hello();
+	console.log("hello, world!");
+}
+)();
