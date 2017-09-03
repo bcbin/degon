95285a9b5d92d9aa83228726b30f4b226ace6bac
bcbin <bcbin@yahoo.com>
2017-09-03 10:31:32 +0800

    Create hellogit.meta.js

---
+++ hellogit.meta.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name         hellogit
+// @namespace    http://degon.ittun.com
+// @version      0.10
+// @updateURL    http://degon.ittun.com/update.php
+// @description  degon drug drug price maintain
+// @author       bcbon@wo.cn
+// ==/UserScript==
