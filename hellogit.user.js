9782f82892605d446dafbd7b24fb950951f0b8fa
bcbin <bcbin@yahoo.com>
2017-09-03 10:37:00 +0800

    Update hellogit.user.js

--- hellogit.user.js
+++ hellogit.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         hellogit
 // @namespace    http://degon.ittun.com
-// @version      0.2
+// @version      0.3
 // @description  hello golang
 // @author       bcbon@wo.cn
 // @include	 *	
