80f7f8f70d004756b1a4b6f2af73af46cbb4d556
bcbin <bcbin@yahoo.com>
2017-09-03 10:52:39 +0800

    Update hellogit.meta.js

--- hellogit.meta.js
+++ hellogit.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         hellogit
 // @namespace    http://degon.ittun.com
-// @version      0.6
+// @version      0.7
 // @description  degon drug drug price maintain
 // @author       bcbon@wo.cn
 // ==/UserScript==
