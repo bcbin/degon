a21d569f453423ce4301d3d0616191ec2d63aba7
bcbin <bcbin@yahoo.com>
2017-09-03 10:35:29 +0800

    Update hellogit.user.js

--- hellogit.user.js
+++ hellogit.user.js
@@ -10,7 +10,7 @@
 // @grant	GM_deleteValue
 // @grant	GM_listValues
 // @grant	GM_setClipboard
-// @updateURL https://openuserjs.org/meta/bcbin/hellogit.meta.js
+// @updateURL https://raw.githubusercontent.com/bcbin/hellogit/master/hellogit.meta.js
 // ==/UserScript==
 
 (function() {
